44ede85cb849781fd3ad95b458971f0a47c1aa09
LoicCaudron <Loic.CAUDRON@student.umons.ac.be>
2018-05-19 12:42:49 +0200

    Modifs User retour des paramaètres

--- app/src/main/java/com/example/einore/exercice_integre/model/User.java
+++ app/src/main/java/com/example/einore/exercice_integre/model/User.java
@@ -5,6 +5,11 @@ public class User {
     private int id;
     private String name;
     private int pin;
+    private float T_min;
+    private float T_max;
+    private float Humidity;
+    private float battery;
+    private String num;
 
 
     public User() {}
@@ -39,4 +44,43 @@ public class User {
         this.pin = pin;
     }
 
+    public float getT_min() {
+        return T_min;
+    }
+
+    public void setT_min(float t_min) {
+        T_min = t_min;
+    }
+
+    public float getT_max() {
+        return T_max;
+    }
+
+    public void setT_max(float t_max) {
+        T_max = t_max;
+    }
+
+    public float getHumidity() {
+        return Humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        Humidity = humidity;
+    }
+
+    public float getBattery() {
+        return battery;
+    }
+
+    public void setBattery(float battery) {
+        this.battery = battery;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
 }
