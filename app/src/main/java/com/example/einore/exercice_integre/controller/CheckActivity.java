3787cd49225993e71b625ac976bdc4c4212d87f6
LoicCaudron <Loic.CAUDRON@student.umons.ac.be>
2018-05-20 22:13:58 +0200

    agrandissement de l'affichage des nombres

--- app/src/main/java/com/example/einore/exercice_integre/controller/CheckActivity.java
+++ app/src/main/java/com/example/einore/exercice_integre/controller/CheckActivity.java
@@ -6,6 +6,7 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.einore.exercice_integre.R;
 import com.example.einore.exercice_integre.model.User;
@@ -52,49 +53,55 @@ public class CheckActivity extends AppCompatActivity {
                 float temp_min,temp_max,humidity_min, humidity_max, battery_min, battery_max;
                 float temp_check, humidity_check, battery_check;
 
+                try{
+                    phone_num = user.getPhone();
+                    temp_min = user.getT_min();
+                    temp_max = user.getT_max();
+                    humidity_min = user.getHumidity_min();
+                    humidity_max = user.getHumidity_max();
+                    battery_min = user.getBattery_min();
+                    battery_max = user.getBattery_max();
 
-                phone_num = user.getPhone();
-                temp_min = user.getT_min();
-                temp_max = user.getT_max();
-                humidity_min = user.getHumidity_min();
-                humidity_max = user.getHumidity_max();
-                battery_min = user.getBattery_min();
-                battery_max = user.getBattery_max();
-
-                temp_check = Float.parseFloat(temperature.getText().toString());
-                humidity_check = Float.parseFloat(humidity.getText().toString());
-                battery_check = Float.parseFloat(battery.getText().toString());
+                    temp_check = Float.parseFloat(temperature.getText().toString());
+                    humidity_check = Float.parseFloat(humidity.getText().toString());
+                    battery_check = Float.parseFloat(battery.getText().toString());
 
 
-                if(temp_check<temp_min){
-                    String message = "Tourne un peu la vanne du radiateur. Il ne fait que " + temp_check + "°C";
-                    SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
+                    if(temp_check<temp_min){
+                        String message = "Tourne un peu la vanne du radiateur. Il ne fait que " + temp_check + "°C";
+                        SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
+                        Toast.makeText(CheckActivity.this, "test réussi",Toast.LENGTH_LONG).show();
+                    }
+
+                    if(temp_check>temp_max){
+                        String message = "Ouvre un peu une fenêtre. Il fait chaud ici! Il fait " + temp_check +"°C";
+                        SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
+                    }
+
+                    if(humidity_check<humidity_min){
+                        String message = "Il fait sec ici:" + humidity_check + "%. Ouvre une fenêtre pour aérer un peu.";
+                        SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
+                    }
+
+                    if(humidity_check>humidity_max){
+                        String message = "Il fait tellement humide ici:" + humidity_check + "% que je vais croire qu'il pleut à l'intérieur.";
+                        SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
+                    }
+
+                    if(battery_check<battery_min){
+                        String message = "Il est temps de changer ma batterie. Je suis à plat. Il ne me reste que " + battery_check + "%.";
+                        SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
+                    }
+
+                    if(battery_check>battery_max){
+                        String message = "Je ne sais pas ce qu'il m'arrive. Je suis survolté, ma batterie atteint " + battery_check + "%.";
+                        SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
+                    }
+                }
+                catch(Exception e){
+                    Toast.makeText(CheckActivity.this, e.toString(),Toast.LENGTH_LONG).show();
                 }
 
-                if(temp_check>temp_max){
-                    String message = "Ouvre un peu une fenêtre. Il fait chaud ici! Il fait " + temp_check +"°C";
-                    SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
-                }
-
-                if(humidity_check<humidity_min){
-                    String message = "Il fait sec ici:" + humidity_check + "%. Ouvre une fenêtre pour aérer un peu.";
-                    SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
-                }
-
-                if(humidity_check>humidity_max){
-                    String message = "Il fait tellement humide ici:" + humidity_check + "% que je vais croire qu'il pleut à l'intérieur.";
-                    SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
-                }
-
-                if(battery_check<battery_min){
-                    String message = "Il est temps de changer ma batterie. Je suis à plat. Il ne me reste que " + battery_check + "%.";
-                    SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
-                }
-
-                if(battery_check>battery_max){
-                    String message = "Je ne sais pas ce qu'il m'arrive. Je suis survolté, ma batterie atteint " + battery_check + "%.";
-                    SmsManager.getDefault().sendTextMessage(phone_num, null, message, null, null );
-                }
 
             }
 
